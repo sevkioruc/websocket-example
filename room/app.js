6943601ffbe0846a141257a0f349aaa0dc923ff8
Sevki <sevkioruc34@gmail.com>
2020-03-29 01:32:48 +0300

    leave room

--- room/app.js
+++ room/app.js
@@ -11,8 +11,23 @@ const io = socketio.listen(server);
 io.on('connection', (socket) => {
     socket.on('join-room', (data) => {
         socket.join(data.name, () => {
-            let userCount = io.sockets.adapter.rooms[data.name].length;
-            io.to(data.name).emit('new-join', {userCount});
+            io.to(data.name).emit('new-join', { count: getOnlineCount(io, data) });
         });
     });
+
+    socket.on('leave-room', (data) => {
+        socket.leave(data.name, () => {
+            io.to(data.name).emit('leavedRoom', { count: getOnlineCount(io, data) });
+        });
+    });
+
 });
+
+const getOnlineCount = (io, data) => {
+    const room = io.sockets.adapter.rooms[data.name];
+    if (room) {
+        return room.length;
+    } else {
+        return 0;
+    }
+}
